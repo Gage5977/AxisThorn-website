df31e67c52ee74f85d09d329382d646f1e0d34ba
Gage5977 <galleman@axisthorn.com>
2025-06-18 03:40:21 -0700

    Fix API function exports and Vercel configuration for proper deployment

--- api/customers/index.js
+++ api/customers/index.js
@@ -23,7 +23,7 @@ class Customer {
   }
 }
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
--- api/payment-methods/index.js
+++ api/payment-methods/index.js
@@ -50,7 +50,7 @@ const paymentMethods = [
   }
 ];
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
